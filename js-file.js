e96cc16100e36725afc02cf8eecb69ab73616420
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-29 16:12:01 +0100

    Use flexbox to remove gap between rows of gridSquares
    In js-file.js:
    - Make the containerDiv a flex container with wrapping enabled
    - Unrelated: make clear that border widths are 3px

--- js-file.js
+++ js-file.js
@@ -1,12 +1,12 @@
 const containerDiv = document.querySelector('div')
+containerDiv.style.display = 'flex'
+containerDiv.style.flexWrap = 'wrap'
 
 for (i = 0; i < 16; i++) {
     const gridSquare = document.createElement('div');
-    
     gridSquare.style.width = '100px';
     gridSquare.style.height = '100px';
-    gridSquare.style.border = 'solid black';
-    gridSquare.style.display = 'inline-block';
+    gridSquare.style.border = 'solid black 3px';
 
     containerDiv.appendChild(gridSquare);
 }
