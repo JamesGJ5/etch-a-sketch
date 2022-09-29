db41e16ee55ba7cc166d4bf3e1664cce2fc11708
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-29 17:42:56 +0100

    Change grid proportion variable name for clarity

--- js-file.js
+++ js-file.js
@@ -2,8 +2,8 @@ const containerDiv = document.querySelector('div');
 containerDiv.style.display = 'flex';
 containerDiv.style.flexWrap = 'wrap';
 
-percentageOfWindow = 100;
-containerDiv.style.width = `${percentageOfWindow}vmin`;
+gridPercentageOfWindow = 100;
+containerDiv.style.width = `${gridPercentageOfWindow}vmin`;
 containerDiv.style.height = containerDiv.style.width
 
 const squaresPerRow = 16;
@@ -12,7 +12,7 @@ for (i = 0; i < squaresPerRow ** 2; i++) {
     const gridSquare = document.createElement('div');
 
     gridSquare.style.boxSizing = 'border-box';
-    gridSquare.style.width = `${percentageOfWindow / squaresPerRow}vmin`;
+    gridSquare.style.width = `${gridPercentageOfWindow / squaresPerRow}vmin`;
     gridSquare.style.height = gridSquare.style.width;
     gridSquare.style.border = 'solid black 3px';
 
