8136b4483bd00e30c0e3aa17069b9cb193e28386
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-29 17:46:48 +0100

    Remove margin from body and add semi-colons where missing

--- js-file.js
+++ js-file.js
@@ -1,10 +1,13 @@
+const body = document.querySelector('body');
+body.style.margin = '0';
+
 const containerDiv = document.querySelector('div');
 containerDiv.style.display = 'flex';
 containerDiv.style.flexWrap = 'wrap';
 
 gridPercentageOfWindow = 100;
 containerDiv.style.width = `${gridPercentageOfWindow}vmin`;
-containerDiv.style.height = containerDiv.style.width
+containerDiv.style.height = containerDiv.style.width;
 
 const squaresPerRow = 16;
 for (i = 0; i < squaresPerRow ** 2; i++) {
