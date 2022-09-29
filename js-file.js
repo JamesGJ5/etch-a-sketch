3333ab03e87152555f295582a787be061dc2c50f
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-29 16:28:38 +0100

    Arrange the gridSquares into a 16x16 grid
    In js-file.js:
    - Allow the grid (containerDiv) to take up 100% of viewport width
    - Initialise a squaresPerRow constant as 16 and make sure the for loop
    only goes while i < the square of this constant
    - Make width and height of each grid square equal to 100% of the
    viewport width divided by squaresPerRow

--- js-file.js
+++ js-file.js
@@ -2,10 +2,17 @@ const containerDiv = document.querySelector('div')
 containerDiv.style.display = 'flex'
 containerDiv.style.flexWrap = 'wrap'
 
-for (i = 0; i < 16 ** 2; i++) {
+containerViewportWidthProportion = 100;
+containerDiv.style.width = `${containerViewportWidthProportion}vw`
+
+const squaresPerRow = 16
+for (i = 0; i < squaresPerRow ** 2; i++) {
+    
     const gridSquare = document.createElement('div');
-    gridSquare.style.width = '100px';
-    gridSquare.style.height = '100px';
+
+    gridSquare.style.boxSizing = 'border-box';
+    gridSquare.style.width = `${containerViewportWidthProportion / squaresPerRow}vw`;
+    gridSquare.style.height = `${containerViewportWidthProportion / squaresPerRow}vw`;
     gridSquare.style.border = 'solid black 3px';
 
     containerDiv.appendChild(gridSquare);
