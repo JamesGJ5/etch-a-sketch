8627338c0db21f276c543b4d0ca2ecf9e51f678f
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-29 20:33:20 +0100

    Add semi-colons where apt for good practice

--- js-file.js
+++ js-file.js
@@ -8,30 +8,30 @@ function styleGrid(grid) {
     grid.style.flexWrap = 'wrap';
     grid.style.width = '100vmin';
     grid.style.height = grid.style.width;
-}
+};
 
 function resetGridSquares(grid) {
     let child = grid.lastElementChild;
     while (child) {
         grid.removeChild(child);
         child = grid.lastElementChild;
-    }
-}
+    };
+};
 
 function styleGridSquare(gridSquare) {
     gridSquare.style.boxSizing = 'border-box';
     gridSquare.style.width = `${100 / squaresPerRow}vmin`;
     gridSquare.style.height = gridSquare.style.width;
     gridSquare.style.border = 'solid black 1px';
-}
+};
 
 function createGridSquares(grid, squaresPerRow) {
     for (i = 0; i < squaresPerRow ** 2; i++) {
         const gridSquare = document.createElement('div');
-        styleGridSquare(gridSquare)
+        styleGridSquare(gridSquare);
         grid.appendChild(gridSquare);
     }
-}
+};
 
 function makeEtchable(grid) {
     const gridSquareList = grid.childNodes;
@@ -41,20 +41,20 @@ function makeEtchable(grid) {
             gridSquare.style.backgroundColor = 'black';
         });
     });
-}
+};
 
 function createGrid(squaresPerRow) {
     // TODO: vertically-center the grid in the window
     const grid = document.querySelector('div');
     styleGrid(grid);
     resetGridSquares(grid);
-    createGridSquares(grid, squaresPerRow)
+    createGridSquares(grid, squaresPerRow);
     makeEtchable(grid);
 };
 
 const button = document.querySelector('button');
 
-let squaresPerRow
+let squaresPerRow;
 button.addEventListener('click', () => {
     squaresPerRow = Infinity;
     while (squaresPerRow > 100) {
