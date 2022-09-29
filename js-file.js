8270cf36390bb8099258f3b0305a1d608f391e3e
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-29 15:54:23 +0100

    Create a single grid square to start
    In js-file.js:
    - Initialise a const for the grid square div
    - Give it a fixed width and height of 100px each
    - Give it a solid black border with a default border-width
    - Append it to containerDiv's child nodelist

--- js-file.js
+++ js-file.js
@@ -1,1 +1,8 @@
 const containerDiv = document.querySelector('div')
+
+const gridSquare = document.createElement('div')
+gridSquare.style.width = '100px'
+gridSquare.style.height = '100px'
+gridSquare.style.border = 'solid black'
+
+containerDiv.appendChild(gridSquare)
