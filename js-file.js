88520d7233bbd7e24d359f2c9fe825e0091781ed
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-29 16:03:24 +0100

    Put 16 grid squares onto page with for loop and use inline-block for rows

--- js-file.js
+++ js-file.js
@@ -1,8 +1,12 @@
 const containerDiv = document.querySelector('div')
 
-const gridSquare = document.createElement('div')
-gridSquare.style.width = '100px'
-gridSquare.style.height = '100px'
-gridSquare.style.border = 'solid black'
+for (i = 0; i < 16; i++) {
+    const gridSquare = document.createElement('div');
+    
+    gridSquare.style.width = '100px';
+    gridSquare.style.height = '100px';
+    gridSquare.style.border = 'solid black';
+    gridSquare.style.display = 'inline-block';
 
-containerDiv.appendChild(gridSquare)
+    containerDiv.appendChild(gridSquare);
+}
